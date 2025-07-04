7c74fdc7633fedfdab56b6fbc271ca9d30fb23f1
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-07-04 14:40:55 -0300

    atualização

---
+++ aula04/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+console.log(`a velocidade do seu  carro é ${vel} K/h`)
+if (vel > 60) {
+     console.log('você foi .MULTADO')
+}
+console.log('Dirija sempre com segurança')
+
+ 
+
---
+++ aula04/ex009.js
@@ -0,0 +1,10 @@
+ var idade = 16
+ console.log(`Olá tenho ${idade} anos posso tira minha CARTEIRA MOTORISTA?`)
+ if (idade >= 18) {
+    console.log('Você é de MAIOR DE IDADE')
+    console.log('pode tira carteira de MOTORISTA')
+ }
+ else {
+    console.log('Você é de MENOR DE IDADE')
+    console.log('da não pode tira carteira MOTORISTA.')
+ }
