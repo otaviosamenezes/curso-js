8f1a129dca5c19b845d1171b7a839350d6719259
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-07-15 23:52:25 -0300

    atualização

--- exe010/script.js
+++ exe010/script.js
@@ -1,13 +1,13 @@
 function somar() {
-    var numero = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    var num = Number(numero.value)
+    let numero = document.getElementById('txtano')
+    let res = document.getElementById('res')
+    let num = Number(numero.value)
     res.innerHTML = ''
     if (numero.value.length == 0) {
       window.alert('porfavor digite um numero')  
     }
     else {
-       var i = 1
+       let i = 1
         while (i <= 10) {
             res.innerHTML += `${num} X ${i} = ${num * i}</br>`
             i++
--- exe09/script.js
+++ exe09/script.js
@@ -1,23 +1,42 @@
 function somar() {
-    var prim = document.getElementById('txtano')
-    var fim = document.getElementById('txtane')
-    var passe = document.getElementById('passe')
-    var res = document.getElementById('res')
+    let prim = document.getElementById('txtano')
+    let fim = document.getElementById('txtane')
+    let passe = document.getElementById('passe')
+    let res = document.getElementById('res')
 
-    if (prim.value.length == 0 || fim.value.length == 0) {
-        window.alert('porfavo digite o número')
+    if (prim.value.length == 0) {
+        window.alert('porfavo digite o INICIO:')
+        res.innerHTML = '[ERRO...]'
     }
     else if (passe.value.length == 0) {
-        window.alert('porfavor coloque o passo')
+        window.alert('porfavor coloque o PASSO:')
+         res.innerHTML = '[ERRO...]'
     }
+    else if (fim.value.length == 0) {
+        window.alert('porfavor coloque o FIM:')
+         res.innerHTML = '[ERRO...]'
+    }
+
     else {
-        var prime = Number(prim.value)
-        var fimm = Number(fim.value)
-        var passi = Number(passe.value)
+        let prime = Number(prim.value)
+        let fimm = Number(fim.value)
+        let passi = Number(passe.value)
+      if (passi <= 0) {
+        window.alert('passo invalido, comsiderando passo 1')
+    }
         res.innerHTML = ''
-        while (prime <= fimm) {
+       if (prime < fimm) {
+        while(prime <= fimm) {
             res.innerHTML += `👉${prime}`
             prime += passi
         }
+       }
+       else {
+        while (prime >= fimm) {
+            res.innerHTML += `👉${prime}`
+            prime -= passi
+        }
+       }
+        res.innerHTML += `🏴`
     }
 }
