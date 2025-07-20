36ff9ece423c4bd04146caa40534ee70b54ab19b
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-07-20 11:00:51 -0300

    atualização

--- praticando010/script.js
+++
@@ -1,2 +0,0 @@
-let cont = 1
-let qnd = prompt("quntos alunos tem na turma?");
--- praticando08/script.js
+++ praticando08/script.js
@@ -1,22 +1,33 @@
 function verificar() {
-    let idade = document.getElementById('txtano')
+    let numero = document.getElementById('txtano')
     let res = document.getElementById('res')
-    let idades = Number(idade.value)
-    
-    if (idade.value.length == 0) {
-        window.alert('porfavo digite sua idade')
+    let data = new Date()
+    let ano = data.getFullYear()
+     let n = Number(numero.value)
+    if (numero.value.length == 0 || numero.value > ano) {
+        window.alert('Porfavor verifica os dados tente novamente. ')
     }
     else {
-        if (idades <= 15) {
-            res.innerHTML = `Você tem ${idades}, não pode vota.`
-        }
-        else if (idades < 18 || idades > 65) {
-            res.innerHTML = `Você tem ${idades}, pode votar. </br>
-            (voto opicional)`
-        }
-        else if (idades < 50) {
-            res.innerHTML = `Você tem ${idades}, pode votar. </br>
-            (voto obrigatorio)`
-        }
+        let idade = ano - n
+        let texto = document.createElement('p')
+        texto.setAttribute('id', 'idade')
+        texto.innerHTML = `Você tem ${idade} anos`
+         let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+         if (idade <= 10) {
+            img.setAttribute('src', 'bebeE.png')
+         }
+         else if (idade <= 29) {
+            img.setAttribute('src', 'jovemE.png')
+         }
+         else if (idade <= 60) {
+            img.setAttribute('src', 'adultoE.png')
+         }
+         else {
+            img.setAttribute('src', 'velhooE.png')
+         }
+         res.innerHTML = ''
+         res.appendChild(texto)
+          res.appendChild(img)
     }
 }
