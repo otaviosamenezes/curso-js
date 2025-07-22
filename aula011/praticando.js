f336249d2e31789ce49c2976d03c79d94bf80119
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-07-22 09:46:20 -0300

    atualização

--- aula011/praticando.js
+++ aula011/praticando.js
@@ -1,27 +1,74 @@
-let num = [7, 4, 9, 3, 2]
-num[5] = 8
-num.sort()
-let n = num.length
-console.log(num)
-console.log(`são ${n} posições`)
+let bum = [5, 8, 3, 9, 4]
+bum.push(1)
+bum.sort()
+let p = bum.length
+console.log(bum)
 
-// for(let pos = 0;pos < num.length;pos ++) {
-//     console.log(`as posições ${pos} os valores ${num[pos]}`)
-// } 
+console.log('')
 
-// for(let pos in num) {
-//     console.log(`posição ${pos} os valores ${num[pos]}`)
-// }
+console.log(`total de elementos é ${p}`)
 
-let pos = 0
-while (pos < num.length) {
-    console.log(`posições ${pos} valores ${num[pos]}`)
-    pos ++
+console.log('forma burra ⬇️')
+
+console.log(bum[0])
+console.log(bum[1])
+console.log(bum[2])
+console.log(bum[3])
+console.log(bum[4])
+console.log(bum[5])
+
+
+console.log('for ⬇️')
+
+
+
+for(let pos = 0;pos < bum.length;pos ++) {
+    console.log(`posição ${pos} elementos ${bum[pos]}`)
 }
-let b = num.indexOf(9)
-if (b == -1) {
-    console.log('Não existe esse número')
+
+
+console.log('while ⬇️')
+
+
+let gs = 0
+while (gs < bum.length) {
+    console.log(`elementos ${bum[gs]} posições ${gs}`)
+    gs ++
 }
-else {
-    console.log(`valor encontrado foi ${b}`)
+
+
+console.log('do ⬇️')
+
+
+let fa = 0
+do {
+    console.log(`posição ${fa} elementos ${bum[fa]}`) 
+    fa ++
+}while(fa < bum.length)
+
+
+console.log('for:simplis ⬇️')
+
+
+for(let fus in bum) {
+    console.log(`elementos ${bum[fus]} posição ${fus}`)
 }
+
+console.log('')
+
+console.log('Buacar valor dentro de um vetor⬇️')
+let v = bum.indexOf(4)
+console.log(`valor de 4 é : ${v}`)
+
+console.log('')
+
+console.log('Buscando valor dentro do vetor usando ➡️condições⬅️')
+
+let vs = bum.indexOf(7)
+if (vs == -1) {
+    console.log('Não existe esse número.')
+} else {
+    console.log(`valor emcontrado foi: ${vs}`)
+}
+
+console.log('')
