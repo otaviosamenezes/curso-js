699939caec59255aac5a464ed3b1fc889a4f2fb4
Otavio Samenezes <otaviogomessamenezes@gmail.com>
2025-08-01 17:32:43 -0300

    atualização

---
+++ exe013/script.js
@@ -0,0 +1,8 @@
+let numero = document.getElementById('txtano')
+let select = document.getElementById('flista')
+let res = document.getElementById('res')
+let vetores = []
+
+function adicionar() {
+
+}
